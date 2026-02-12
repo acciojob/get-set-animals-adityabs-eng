a3021138690cbe915a79bf1cb448d29a8edd5762
Aditya Bhushan <144267454+adityabs-eng@users.noreply.github.com>
2026-02-13 03:19:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 //complete this code
-class Animal {}
+class Animal {
+	constructor(species : string){
+		this.animalSpecies = species;
+	}
+	get species : string (){
+		return this.animalSpecies;
+	}
+	makeSound (){
+		console.log(`The ${this.animalSpecies} makes a sound`)
+	}
+	
+}
 
-class Dog extends Animal {}
+class Dog extends Animal {
+	constructor(species){
+		super(species);
+	}
+	bark(): void {
+	    console.log("woof");
+  }
+}
 
-class Cat extends Animal {}
+class Cat extends Animal {
+	constructor(species){
+		super(species);
+	}
+	purr(): void {
+	    console.log("purr");
+  }
+}
 
 // Do not change the code below this line
 window.Animal = Animal;
