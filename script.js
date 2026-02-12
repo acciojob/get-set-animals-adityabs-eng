507594fe84da727436a5797b5ff4b09b84afe3c4
Aditya Bhushan <144267454+adityabs-eng@users.noreply.github.com>
2026-02-13 03:22:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,33 +1,38 @@
 //complete this code
 class Animal {
-	constructor(species : string){
+	private animalSpecies: string;
+
+	constructor(species: string){
 		this.animalSpecies = species;
 	}
-	get species : string (){
+
+	get species(): string {
 		return this.animalSpecies;
 	}
-	makeSound (){
-		console.log(`The ${this.animalSpecies} makes a sound`)
+
+	makeSound(): void {
+		console.log(`The ${this.animalSpecies} makes a sound`);
 	}
-	
 }
 
 class Dog extends Animal {
-	constructor(species){
+	constructor(species: string){
 		super(species);
 	}
+
 	bark(): void {
 	    console.log("woof");
-  }
+    }
 }
 
 class Cat extends Animal {
-	constructor(species){
+	constructor(species: string){
 		super(species);
 	}
+
 	purr(): void {
 	    console.log("purr");
-  }
+    }
 }
 
 // Do not change the code below this line
